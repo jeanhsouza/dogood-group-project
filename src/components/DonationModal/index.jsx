e3054f72eec37e47cb4ed63921ced33f593ec112
renangiaretta <ranan.giaretta@hotmail.com>
2023-01-05 13:37:11 -0300

    feature/creteDonationModal

---
+++ src/components/DonationModal/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StyledDonationModal } from './style'
+
+const DonationModal = ( { openModal } ) => {
+  return (
+    <StyledDonationModal>
+        <div className='modalWrapper'>
+            <div className='headerContainer'>
+                <div className='titleContainer'>
+                    <h1 className='title'>NOME DA ONG</h1>
+                </div>
+                <button className='closeButton' onClick={openModal} >X</button>
+            </div>
+            <h2 className='descriptionContainer'>Formulário de doação</h2>
+            <form className='donationForm' action="submit" noValidate>
+                <input type="text" placeholder='Nome'/>
+                <input type="text" placeholder='Telefone'/>
+                <input type="text" placeholder='Número do cartão'/>
+                <input type="text" placeholder='E-mail'/>
+                <input type="text" placeholder='Quanto deseja doar?'/>
+                <input className='cardInput' type="text" placeholder='MM/YY'/>
+                <input className='cardInput' type="text" placeholder='CVC'/>
+            </form>
+            <button className='donationButton'>DOAR AGORA</button>
+        </div>
+    </StyledDonationModal>
+  )
+}
+
+export default DonationModal
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import DonationModal from "../../components/DonationModal";
 import { StyledLogin } from "./style";
 
 const Login = () => {
+
+    const [ showModal, setShowModal ] = useState(false)
+    function openModal () {
+        setShowModal(!showModal)
+    }
+
 	return (<StyledLogin>
-<h1>Login</h1>
+        <h1>Login</h1>
+        <button onClick={openModal}>showModal</button>
+        {(showModal ? <DonationModal openModal={openModal} /> : null)}
     </StyledLogin>);
 };
 
