932e9ad522d7cd05eeff24acb1b4b02573b874d8
glima-dev <guilherme.lima.tec@live.com>
2023-01-06 03:27:23 -0300

    feat: createProfileEditForm

--- src/pages/dashboard/components/DashControlPanel/index.jsx
+++ src/pages/dashboard/components/DashControlPanel/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BiPlus, BiEditAlt, BiLogOut, BiUserCircle } from "react-icons/bi";
 import { StyledButton } from "../../../../styles/buttons";
+import DashProfileForm from "./DashProfileForm";
 import { StyledDashControlPanel } from "./style";
 
 const DashControlPanel = () => {
@@ -12,27 +13,30 @@ const DashControlPanel = () => {
     return (
         <StyledDashControlPanel>
             <div>
-                <StyledButton buttonSize="default" buttonStyle="primaryDefault" >
-                    <span>
-                        <BiUserCircle />
-                    </span>
-                </StyledButton>
-                <StyledButton buttonSize="default" buttonStyle="primaryActive" >
-                    <span>
-                        <BiPlus />
-                    </span>
-                </StyledButton>
-                <StyledButton buttonSize="default" buttonStyle="primaryDefault" >
-                    <span>
-                        <BiLogOut />
-                    </span>
-                </StyledButton>
+                <div>
+                    <StyledButton buttonSize="default" buttonStyle="primaryDefault" >
+                        <span>
+                            <BiUserCircle />
+                        </span>
+                    </StyledButton>
+                    <StyledButton buttonSize="default" buttonStyle="primaryActive" >
+                        <span>
+                            <BiPlus />
+                        </span>
+                    </StyledButton>
+                    <StyledButton buttonSize="default" buttonStyle="primaryDefault" >
+                        <span>
+                            <BiLogOut />
+                        </span>
+                    </StyledButton>
 
+                </div>
+                <div>
+                    <h2>META: 40.000$</h2>
+                    <h2>ARRECADADO: 60.000$</h2>
+                </div>
             </div>
-            <div>
-                <h2>META: 40.000$</h2>
-                <h2>ARRECADADO: 60.000$</h2>
-            </div>
+            <DashProfileForm />
 
         </StyledDashControlPanel>
     );
--- src/pages/dashboard/components/DashControlPanel/style.js
+++ src/pages/dashboard/components/DashControlPanel/style.js
@@ -2,43 +2,50 @@ import styled from "styled-components";
 
 export const StyledDashControlPanel = styled.section`
 	/* animation: slideLeft 0.4s ease forwards; */
-    margin: 32px auto 0;
-    display: flex;
-    flex-direction: column;
-    gap: 32px;
     height: max-content;
     width: 92%;
     max-width: 1000px;
-
-    @media (min-width: 500px) {
-        flex-direction: row-reverse;
-        justify-content: space-between;
-    }
-
-    @media (min-width: 900px) {
-
-    }
+    margin: 32px auto 0;
 
 
-    & > div:first-child{
+    & > div:nth-child(1){
         display: flex;
-        gap: 18px;
-    }
+        flex-direction: column;
+        gap: 32px;
+        height: max-content;
+        width: 100%;
 
-    & > div:last-child{
-        h2{
-            color: var(--primary100);
-            font-size: var(--font-size-1);
-            font-family: var(--font-family-1);
-            letter-spacing: .5px;
-            line-height: var(--line-height);
-            font-weight: var(--font-weight-3);
 
-            @media (min-width: 900px) {
-                font-size: var(--font-size-0);
-            }
+        @media (min-width: 500px) {
+            flex-direction: row-reverse;
+            justify-content: space-between;
+        }
+
+        @media (min-width: 900px) {
 
         }
+
+
+        & > div:first-child{
+            display: flex;
+            gap: 18px;
+        }
+
+        & > div:last-child{
+            h2{
+                color: var(--primary100);
+                font-size: var(--font-size-1);
+                font-family: var(--font-family-1);
+                letter-spacing: .5px;
+                line-height: var(--line-height);
+                font-weight: var(--font-weight-3);
+
+                @media (min-width: 900px) {
+                    font-size: var(--font-size-0);
+                }
+
+            }
     }
+}
 
 `;
