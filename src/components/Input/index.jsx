92b736a592dedd3ec60e9f54dff389d86a94d8e0
Jean Souza <jeanhsouza@gmail.com>
2023-01-11 13:46:41 -0300

    fix:Input

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,13 @@
+import { StyledInput, StyledP, Wrapper } from "./style";
+import { forwardRef } from "react";
+
+export const Input = forwardRef(({ name, error, ...rest }, ref) => {
+	return (
+		<>
+			<Wrapper>
+				<StyledInput name={name} ref={ref} {...rest} />
+				{error && <StyledP>{error[name]?.message}</StyledP>}
+			</Wrapper>
+		</>
+	);
+});
